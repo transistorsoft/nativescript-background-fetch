9c733857afa3b6cf9b54fcf31cc9db7152935435
Chris Scott <christocracy@gmail.com>
2017-10-04 08:39:24 -0400

    Forgot to implement empty status method for Android facade

--- background-fetch.android.d.ts
+++ background-fetch.android.d.ts
@@ -1,10 +1,8 @@
 export declare class BackgroundFetch {
-    static FETCH_RESULT_NEW_DATA: number;
-    static FETCH_RESULT_NO_DATA: number;
-    static FETCH_RESULT_FAILED: number;
     private static configured;
     static configure(config?: Object, callback?: Function, failure?: Function): void;
     static start(success?: Function, failure?: Function): void;
     static stop(success?: Function, failure?: Function): void;
     static finish(result?: number): void;
+    static status(success: Function): void;
 }
--- background-fetch.android.ts
+++ background-fetch.android.ts
@@ -1,24 +1,13 @@
 var emptyFn = function() {};
 
 export class BackgroundFetch {
-  public static FETCH_RESULT_NEW_DATA = 0;
-  public static FETCH_RESULT_NO_DATA = 1;
-  public static FETCH_RESULT_FAILED = 2;
-
   private static configured: boolean;
 
   public static configure(config?:Object, callback?:Function, failure?:Function) {
-    console.warn('BackgroundFetch#configure has no Android implementation');
+    console.warn('BackgroundFetch has no Android implementation');
   }
-  public static start(success?:Function, failure?:Function) {
-    console.warn('BackgroundFetch#start has no Android implementation');
-  }
-
-  public static stop(success?:Function, failure?:Function) {
-    console.warn('BackgroundFetch#stop has no Android implementation');
-  }
-
-  public static finish(result?:number) {
-    console.warn('BackgroundFetch#finish has no Android implementation');
-  }
+  public static start(success?:Function, failure?:Function) {}
+  public static stop(success?:Function, failure?:Function) {}
+  public static finish(result?:number) {} 
+  public static status(success:Function) {}
 }
